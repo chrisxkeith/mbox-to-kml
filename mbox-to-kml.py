14a8cb08f704859ec41f392aa0a4245857ff1880
Chris Keith <chris.keith@gmail.com>
2022-06-21 17:26:37 -0700

    Display data about parts.

--- mbox-to-kml.py
+++ mbox-to-kml.py
@@ -4,15 +4,21 @@ import time
 start = time.time()
 mbox = mailbox.mbox(r"2020-10-emails.mbox")
 for i, message in enumerate(mbox):
-    end = time.time()
-    print(float(end - start))
-    print("from   :",message['from'])
-    print("subject:",message['subject'])
+    if i == 0:
+        end = time.time()
+        print(str(end - start) + ' seconds')
+    print("from   :", message['from'])
+    print("subject:", message['subject'])
     if message.is_multipart():
-        content = 'multipart' # ''.join(part.get_payload(decode=True) for part in message.get_payload())
+        content = 'multipart'
+        for part in message.get_payload():
+            s = str(i) + ': ' + part._default_type
+            if part._default_type == 'text/plain':
+                s += ': ' + part.as_string()[:60]
+            print(s)
     else:
         content = message.get_payload(decode=True)
-    print("content:",content)
+    print("content:", content)
     print("**************************************")
 
     if i == 2:
